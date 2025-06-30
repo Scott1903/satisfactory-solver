e2113fbf6d6684f3ee5dc1f356784714dcabe1ac
Scott Wright <scott.wright@landlineco.com>
2025-06-30 16:39:02 -0500

    Fix build folder dependency in yml file

--- optimizer_backend/settings.py
+++ optimizer_backend/settings.py
@@ -127,4 +127,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
