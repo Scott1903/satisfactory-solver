5e51184805a60484f3be33b3d16d55f6eabad4c4
Scott Wright <scott.wright@landlineco.com>
2025-06-30 15:52:10 -0500

    Fix static files settings

--- optimizer_backend/urls.py
+++ optimizer_backend/urls.py
@@ -16,3 +16,8 @@ urlpatterns = [
     # Catch-all for React routes (must come last)
     re_path(r'^.*$', FrontendAppView.as_view(), name='frontend'),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
